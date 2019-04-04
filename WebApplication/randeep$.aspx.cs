3258db0abf40e7181eaf92720765ae24239cbc54
Randeep Kaur <731777@student.tor.loc>
2019-04-04 17:22:18 -0400

    last commit of the day

---
+++ WebApplication/randeep$.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+   
+
+        public partial class gagan : System.Web.UI.Page
+        {
+            protected void Page_Load(object sender, EventArgs e)
+            {
+                if (!Page.IsPostBack)
+                {
+                    ltMessage.Text = "Welcome to the gagan page";
+                    ltMessage.Text = "You are under my control now";
+                }
+            }
+
+            protected void Button1_Click(object sender, EventArgs e)
+            {
+                string email = TextBox1.Text;
+            }
+        }
+    }
